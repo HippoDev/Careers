8f9625058d01b7d446060b95f592d0bda3199ff4
Nattapon <dongoku18@hotmail.com>
2017-05-29 17:44:29 +0700

    Change server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ var express = require('express')
 var app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  var obj = "sirisoft";
+  res.send('Hello World, Hello ' + obj)
 })
  
-app.listen(3000)
+app.listen(3001)
